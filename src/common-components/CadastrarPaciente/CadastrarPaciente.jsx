2bb94c8e1485db1531ebbff7f785aec4b600766e
Fabricio <fabricio.seb1@gmail.com>
2023-09-26 14:30:04 -0300

    feat: arrow function component on patient registration & fhir integration

--- src/common-components/CadastrarPaciente/CadastrarPaciente.jsx
+++ src/common-components/CadastrarPaciente/CadastrarPaciente.jsx
@@ -1,189 +1,180 @@
-import React, { Component } from 'react';
-import './CadastrarPaciente.css';
-import axios from 'axios';
+import React, { useState } from "react";
+import "./CadastrarPaciente.css";
+import { createPatient } from "../../services/API.service";
 
-class PatientForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            patient: {
-                firstName: '',
-                lastName: '',
-                cpf: '',
-                gender: '',
-                birthDate: '',
-                contactNumber: '',
-            },
-            emergencyContact: {
-                firstName: '',
-                lastName: '',
-                gender: '',
-                relationship: '',
-                emergencyNumber: '',
-            },
-        };
-    }
+const PatientForm = () => {
+  const [state, setState] = useState({
+    patient: {
+      firstName: "",
+      lastName: "",
+      cpf: "",
+      gender: "",
+      birthDate: "",
+      contactNumber: "",
+    },
+    //   emergencyContact: {
+    //     firstName: "",
+    //     lastName: "",
+    //     gender: "",
+    //     relationship: "",
+    //     emergencyNumber: "",
+    //   },
+  });
 
-    handleChange = (e, section) => {
-        const { name, value } = e.target;
-        this.setState((prevState) => ({
-            [section]: {
-                ...prevState[section],
-                [name]: value,
-            },
-        }));
-    };
+  const handleChange = (e, section) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({
+      [section]: {
+        ...prevState[section],
+        [name]: value,
+      },
+    }));
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const { patient, emergencyContact } = this.state;
+    const { patient } = state;
 
+    await createPatient(patient);
 
-        axios.post('', {
-            patient,
-            emergencyContact,
-        })
-            .then((response) => {
+    setState({
+      patient: {
+        firstName: "",
+        lastName: "",
+        cpf: "",
+        gender: "",
+        birthDate: "",
+        contactNumber: "",
+      },
+    });
+  };
 
-            })
-            .catch((error) => {
+  return (
+    <div className="container-cadastrar-paciente">
+      <h1 className="titulo-cadastrar-paciente">Cadastrar Paciente</h1>
+      <form className="form-cadastrar-paciente" onSubmit={handleSubmit}>
+        <div>
+          <label>Primeiro Nome*</label>
+          <input
+            type="text"
+            name="firstName"
+            value={state.patient.firstName}
+            onChange={(e) => handleChange(e, "patient")}
+            required
+          />
+        </div>
+        <div>
+          <label>Último Nome*</label>
+          <input
+            type="text"
+            name="lastName"
+            value={state.patient.lastName}
+            onChange={(e) => handleChange(e, "patient")}
+            required
+          />
+        </div>
+        <div>
+          <label>CPF*</label>
+          <input
+            name="cpf"
+            value={state.patient.cpf}
+            type="text"
+            pattern="\d*"
+            maxLength="11"
+            minLength="11"
+            onChange={(e) => {
+              e.target.value = e.target.value.replace(/[^0-9]/g, "");
+              handleChange(e, "patient");
+            }}
+            required
+          />
+        </div>
+        <div>
+          <label>Gênero*</label>
+          <input
+            type="text"
+            name="gender"
+            value={state.patient.gender}
+            onChange={(e) => handleChange(e, "patient")}
+            required
+          />
+        </div>
+        <div>
+          <label>Data de Nascimento*</label>
+          <input
+            type="text"
+            name="birthDate"
+            value={state.patient.birthDate}
+            onChange={(e) => handleChange(e, "patient")}
+            required
+          />
+        </div>
+        <div>
+          <label>Telefone para Contato*</label>
+          <input
+            type="text"
+            name="contactNumber"
+            value={state.patient.contactNumber}
+            onChange={(e) => {
+              handleChange(e, "patient");
+            }}
+            required
+          />
+        </div>
 
-            });
-
-
-        this.setState({
-            patient: {
-                firstName: '',
-                lastName: '',
-                cpf: '',
-                gender: '',
-                birthDate: '',
-                contactNumber: '',
-            },
-            emergencyContact: {
-                firstName: '',
-                lastName: '',
-                gender: '',
-                relationship: '',
-                emergencyNumber: '',
-            },
-        });
-    };
-
-    render() {
-        const { patient, emergencyContact } = this.state;
-
-        return (
-            <div className='container-cadastrar-paciente'>
-                <h1 className='titulo-cadastrar-paciente'>Cadastrar Paciente</h1>
-                <form className='form-cadastrar-paciente' onSubmit={this.handleSubmit}>
-                    <div>
-                        <label>Primeiro Nome*</label>
-                        <input
-                            type="text"
-                            name="firstName"
-                            value={patient.firstName}
-                            onChange={(e) => this.handleChange(e, 'patient')}
-                        />
-                    </div>
-                    <div>
-                        <label>Último Nome*</label>
-                        <input
-                            type="text"
-                            name="lastName"
-                            value={patient.lastName}
-                            onChange={(e) => this.handleChange(e, 'patient')}
-                        />
-                    </div>
-                    <div>
-                        <label>CPF*</label>
-                        <input
-                            type="text"
-                            name="cpf"
-                            value={patient.cpf}
-                            onChange={(e) => this.handleChange(e, 'patient')}
-                        />
-                    </div>
-                    <div>
-                        <label>Gênero*</label>
-                        <input
-                            type="text"
-                            name="gender"
-                            value={patient.gender}
-                            onChange={(e) => this.handleChange(e, 'patient')}
-                        />
-                    </div>
-                    <div>
-                        <label>Data de Nascimento*</label>
-                        <input
-                            type="text"
-                            name="birthDate"
-                            value={patient.birthDate}
-                            onChange={(e) => this.handleChange(e, 'patient')}
-                        />
-                    </div>
-                    <div>
-                        <label>Telefone para Contato*</label>
-                        <input
-                            type="text"
-                            name="contactNumber"
-                            value={patient.contactNumber}
-                            onChange={(e) => this.handleChange(e, 'patient')}
-                        />
-                    </div>
-
-                    <h3 className='titulo-contato-emergencia'>Contato de Emergência</h3>
-                    <div>
-                        <label>Primeiro Nome*</label>
-                        <input
-                            type="text"
-                            name="firstName"
-                            value={emergencyContact.firstName}
-                            onChange={(e) => this.handleChange(e, 'emergencyContact')}
-                        />
-                    </div>
-                    <div>
-                        <label>Último Nome*</label>
-                        <input
-                            type="text"
-                            name="lastName"
-                            value={emergencyContact.lastName}
-                            onChange={(e) => this.handleChange(e, 'emergencyContact')}
-                        />
-                    </div>
-                    <div>
-                        <label>Gênero*</label>
-                        <input
-                            type="text"
-                            name="gender"
-                            value={emergencyContact.gender}
-                            onChange={(e) => this.handleChange(e, 'emergencyContact')}
-                        />
-                    </div>
-                    <div>
-                        <label>Grau de Parentesco*</label>
-                        <input
-                            type="text"
-                            name="relationship"
-                            value={emergencyContact.relationship}
-                            onChange={(e) => this.handleChange(e, 'emergencyContact')}
-                        />
-                    </div>
-                    <div>
-                        <label>Telefone para Contato*</label>
-                        <input
-                            type="text"
-                            name="emergencyNumber"
-                            value={emergencyContact.emergencyNumber}
-                            onChange={(e) => this.handleChange(e, 'emergencyContact')}
-                        />
-                    </div>
-                    <button className='button-fazer-cadastro' type="submit">FAZER CADASTRO</button>
-                </form>
-            </div>
-        );
-    }
-}
+        {/* <h3 className="titulo-contato-emergencia">Contato de Emergência</h3>
+          <div>
+            <label>Primeiro Nome*</label>
+            <input
+              type="text"
+              name="firstName"
+              value={emergencyContact.firstName}
+              onChange={(e) => handleChange(e, "emergencyContact")}
+            />
+          </div>
+          <div>
+            <label>Último Nome*</label>
+            <input
+              type="text"
+              name="lastName"
+              value={emergencyContact.lastName}
+              onChange={(e) => handleChange(e, "emergencyContact")}
+            />
+          </div>
+          <div>
+            <label>Gênero*</label>
+            <input
+              type="text"
+              name="gender"
+              value={emergencyContact.gender}
+              onChange={(e) => handleChange(e, "emergencyContact")}
+            />
+          </div>
+          <div>
+            <label>Grau de Parentesco*</label>
+            <input
+              type="text"
+              name="relationship"
+              value={emergencyContact.relationship}
+              onChange={(e) => handleChange(e, "emergencyContact")}
+            />
+          </div>
+          <div>
+            <label>Telefone para Contato*</label>
+            <input
+              type="text"
+              name="emergencyNumber"
+              value={emergencyContact.emergencyNumber}
+              onChange={(e) => handleChange(e, "emergencyContact")}
+            />
+          </div> */}
+        <button className="button-fazer-cadastro" type="submit">
+          FAZER CADASTRO
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default PatientForm;
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -17,3 +17,42 @@ export const listBundles = async () => {
       throw error;
     });
 };
+
+export const createPatient = async (patient) => {
+  return api
+    .post("/Patient", {
+      resourceType: "Patient",
+      meta: {
+        profile: [
+          "http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient",
+        ],
+      },
+      identifier: [
+        {
+          system: "cpf",
+          value: patient.cpf,
+        },
+      ],
+      name: [
+        {
+          use: "official",
+          text: `${patient.firstName} ${patient.lastName}`,
+          family: patient.lastName,
+          given: [patient.firstName],
+        },
+      ],
+      telecom: [
+        {
+          system: "phone",
+          value: patient.contactNumber,
+          use: "mobile",
+        },
+      ],
+      gender: patient.gender,
+      birthDate: patient.birthDate,
+    })
+    .then((res) => res.data)
+    .catch((error) => {
+      throw error;
+    });
+};
