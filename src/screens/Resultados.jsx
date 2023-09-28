5ea586b91bb0f0ee5f4870edeea883429f3b8ffa
brunaportelaa <brunaporteladj@gmail.com>
2023-09-27 21:32:56 -0300

    criada página de resultados

---
+++ src/screens/Resultados.jsx
@@ -0,0 +1,15 @@
+import Footer from "../common-components/Footer/Footer";
+import Header from "../common-components/Header/Header";
+import ResultsPage from "../common-components/Resultados/Resultados";
+
+function Resultados() {
+  return (
+    <div className="">
+      <Header />
+      <ResultsPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default Resultados;
