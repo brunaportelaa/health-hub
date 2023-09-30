3608ad18a4b9cec2fe053be29917273d09120ecc
Fabricio <fabricio.seb1@gmail.com>
2023-09-30 18:07:52 -0300

    payload do bundle de criação de prontuario

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -58,11 +58,98 @@ export const createPatient = async (patient) => {
 };
 
 export const getPatientByCpf = async (cpf) => {
-    try {
-        const {data} = await api.get(`/Patient?identifier=cpf|${cpf}`);
+  try {
+    const { data } = await api.get(`/Patient?identifier=cpf|${cpf}`);
 
-        return data;
-    } catch (error) {
-        throw error;
-    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createMedicalRegistration = async (bundle) => {
+  try {
+    const { data } = await api.post(`/Bundle`, {
+      resourceType: "Bundle",
+      type: "document",
+      identifier: [
+        {
+          system: "cpf",
+          value: bundle.cpf,
+        },
+      ],
+      entry: [
+        {
+          resource: {
+            resourceType: "Condition",
+            clinicalStatus: {
+              coding: [
+                {
+                  system: "http://hl7.org/fhir/ValueSet/condition-clinical",
+                  code: bundle.condition.clinicalStatus,
+                },
+              ],
+            },
+            verificationStatus: {
+              coding: [
+                {
+                  system: "http://hl7.org/fhir/ValueSet/condition-ver-status",
+                  code: bundle.condition.verificationStatus,
+                },
+              ],
+            },
+            recordedDate: bundle.condition.recordedDate,
+            abatementDateTime: bundle.condition.abatementDateTime,
+            note: [
+              {
+                text: bundle.condition.note,
+              },
+            ],
+          },
+        },
+        {
+          resource: {
+            resourceType: "Procedure",
+            status: bundle.procedure.status,
+            category: [
+              {
+                coding: [
+                  {
+                    system: "http://hl7.org/fhir/ValueSet/procedure-category",
+                    code: bundle.procedure.category,
+                  },
+                ],
+              },
+            ],
+            note: [
+              {
+                text: bundle.procedure.note,
+              },
+            ],
+            performedDateTime: bundle.procedure.performedDateTime,
+          },
+        },
+        {
+          resource: {
+            resourceType: "MedicationAdministration",
+            status: bundle.medicationAdministration.status,
+            note: [
+              {
+                text: bundle.medicationAdministration.note,
+              },
+            ],
+            effectiveDateTime:
+              bundle.medicationAdministration.effectiveDateTime,
+            dosage: {
+              text: bundle.medicationAdministration.dosage,
+            },
+          },
+        },
+      ],
+    });
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
 };
